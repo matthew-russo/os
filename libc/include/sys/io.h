2f4e07ee3f04fa813d515512b1acfcff36375353
Matthew Russo <matthew@matthewclayrusso.com>
2020-01-07 21:01:02 -0800

    adds _outb libc/sys/io func. I think idt is working but i dont know...

--- libc/include/sys/io.h
+++ libc/include/sys/io.h
@@ -1,31 +1,10 @@
 #ifndef _SYS_IO_H
-#define _SYS_IO_H 1
+#define _SYS_IO_H
 
-unsigned char inb(unsigned short int port);
-unsigned char inb_p(unsigned short int port);
-unsigned short int inw(unsigned short int port);
-unsigned short int inw_p(unsigned short int port);
-unsigned int inl(unsigned short int port);
-unsigned int inl_p(unsigned short int port);
+#include <stdint.h>
 
-void outb(unsigned char value, unsigned short int port);
-void outb_p(unsigned char value, unsigned short int port);
-void outw(unsigned short int value, unsigned short int port);
-void outw_p(unsigned short int value, unsigned short int port);
-void outl(unsigned int value, unsigned short int port);
-void outl_p(unsigned int value, unsigned short int port);
-
-void insb(unsigned short int port, void *addr,
-           unsigned long int count);
-void insw(unsigned short int port, void *addr,
-           unsigned long int count);
-void insl(unsigned short int port, void *addr,
-           unsigned long int count);
-void outsb(unsigned short int port, const void *addr,
-           unsigned long int count);
-void outsw(unsigned short int port, const void *addr,
-           unsigned long int count);
-void outsl(unsigned short int port, const void *addr,
-           unsigned long int count);
+void _outb(uint16_t port, uint8_t value);
+// void outw(unsigned short int port, unsigned short int value);
+// void outl(unsigned int port, unsigned short int value);
 
 #endif
