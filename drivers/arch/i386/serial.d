ec1ef0d1b5817c01abad4575cc848f55c9d7d924
Matthew Russo <matthew@matthewclayrusso.com>
2020-01-12 14:20:24 -0800

    fix Makefiles, deletes unwated files, and adds drivers gitignore

--- drivers/arch/i386/serial.d
+++
@@ -1,9 +0,0 @@
-arch/i386/serial.o: arch/i386/serial.c \
- /Users/matthewrusso/projects/c_cpp/os/deps/compiler/run/lib/gcc/i686-elf/10.0.0/include/stdint.h \
- /Users/matthewrusso/projects/c_cpp/os/deps/compiler/run/lib/gcc/i686-elf/10.0.0/include/stdint-gcc.h \
- /Users/matthewrusso/projects/c_cpp/os/deps/compiler/run/lib/gcc/i686-elf/10.0.0/include/stdbool.h \
- /Users/matthewrusso/projects/c_cpp/os/sysroot/usr/include/string.h \
- /Users/matthewrusso/projects/c_cpp/os/sysroot/usr/include/sys/cdefs.h \
- /Users/matthewrusso/projects/c_cpp/os/deps/compiler/run/lib/gcc/i686-elf/10.0.0/include/stddef.h \
- /Users/matthewrusso/projects/c_cpp/os/sysroot/usr/include/sys/io.h \
- include/drivers/serial.h
